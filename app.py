0585e94688613b2045eebaabc3d78e6f1a167bac
pranav <dev.pranavsg@gmail.com>
2026-07-20 18:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def evaluate(payload: BatchEvalInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host = "0.0.0.0", port = 8000)
+    uvicorn app:app --host 0.0.0.0 --port $PORT
